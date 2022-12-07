65ecc989f7fc9356f85017f6ef8ac4fb68ab2d19
sy4sylvia <sguo713@gmail.com>
2022-12-07 13:13:57 -0500

    chore: remove unused params

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ import tkinter as tk
 from music_visualizer import generate_visualizer
 
 if __name__ == '__main__':
-    params_file = open("config/setting.json", "r")
-    params = json.load(params_file)
-    params_file.close()
 
     my_music_clip = MusicClip()
     mid_file_name = "output.mid"
